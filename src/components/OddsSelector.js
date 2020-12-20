3efd823c66a63f481c4bdb0738710e9cb8814dcd
Mitch HoraceK <mitch@inthezone.dev>
2020-12-19 22:19:00 -0700

    start odds selector

---
+++ src/components/OddsSelector.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const spreadTable = [[0.5, 50], [1, 60]];
+
+const OddsSelector = ( {setOdds, handleOdds }) => {
+const [inputType, setInputType] = useState("percentage")
+
+const lookupSpread = (userInput) => {
+    const spreadPercentage = spreadTable.filter()
+}
+
+if(percentage)
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default OddsSelector
